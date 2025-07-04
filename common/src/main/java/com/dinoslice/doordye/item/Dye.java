1da1bc33653e6facd1beb2df90fcf544927bf262
MSKatKing <mark@dinoslice.com>
2025-07-04 16:41:10 -0500

    Update dye registration and creation

--- common/src/main/java/com/dinoslice/doordye/item/Dye.java
+++ common/src/main/java/com/dinoslice/doordye/item/Dye.java
@@ -1,17 +1,60 @@
 package com.dinoslice.doordye.item;
 
-import net.minecraft.util.ColorRGBA;
+import com.dinoslice.doordye.registry.CommonBlocks;
+import net.minecraft.client.color.block.BlockColor;
+import net.minecraft.client.color.item.ItemColor;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.CarpetBlock;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+
+import java.awt.*;
 
 public class Dye {
-    public static final Dye FUSCHIA = new Dye(0xFF00FF);
+    public static final Dye FUCHSIA = new Dye(0x9D0759, "fuchsia");
+    public static final Dye CELESTE = new Dye(0xB2FFFF, "celeste");
 
-    private final ColorRGBA color;
+    private final Color color;
 
-    public Dye(int hexColor) {
-        color = new ColorRGBA(hexColor << 8 | 0xFF);
+    private final Block woolBlock;
+    private final Block carpetBlock;
+    private final Block terracottaBlock;
+    private final Block concreteBlock;
+
+    public Dye(int hexColor, String id) {
+        this.color = new Color(hexColor);
+
+        this.woolBlock = CommonBlocks.register(id + "_wool", Block::new, BlockBehaviour.Properties.of(), true);
+        this.carpetBlock = CommonBlocks.register(id + "_carpet", CarpetBlock::new, BlockBehaviour.Properties.of(), true);
+        this.terracottaBlock = CommonBlocks.register(id + "_terracotta", Block::new, BlockBehaviour.Properties.of(), true);
+        this.concreteBlock = CommonBlocks.register(id + "_concrete", Block::new, BlockBehaviour.Properties.of(), true);
     }
 
-    public ColorRGBA getColor() {
-        return color;
+    public void registerColors(BlockColorRegistry registry) {
+        registry.register((blockState, blockAndTintGetter, blockPos, i) -> this.color.getRGB(), this.woolBlock, this.concreteBlock, this.terracottaBlock, this.concreteBlock);
+    }
+
+    public void registerColors(ItemColorRegistry registry) {
+        registry.register((stack, i) -> this.color.getRGB(), this.woolBlock.asItem(), this.carpetBlock.asItem(), this.terracottaBlock.asItem(), this.concreteBlock.asItem());
+    }
+
+    public static void registerBlockColor(BlockColorRegistry registry) {
+        FUCHSIA.registerColors(registry);
+        CELESTE.registerColors(registry);
+    }
+
+    public static void registerItemColor(ItemColorRegistry registry) {
+        FUCHSIA.registerColors(registry);
+        CELESTE.registerColors(registry);
+    }
+
+    public static void registerDyes() { }
+
+    public interface BlockColorRegistry {
+        void register(BlockColor color, Block... blocks);
+    }
+
+    public interface ItemColorRegistry {
+        void register(ItemColor color, Item... items);
     }
 }
---
+++ common/src/main/java/com/dinoslice/doordye/registry/CommonBlocks.java
@@ -0,0 +1,26 @@
+package com.dinoslice.doordye.registry;
+
+import com.dinoslice.doordye.Constants;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+
+import java.util.function.Function;
+
+public class CommonBlocks {
+    public static Block register(String id, Function<BlockBehaviour.Properties, Block> blockFactory, BlockBehaviour.Properties settings, boolean shouldRegistryItem) {
+        ResourceLocation key = ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, id);
+        Block block = blockFactory.apply(settings);
+
+        if (shouldRegistryItem) {
+            BlockItem item = new BlockItem(block, new Item.Properties());
+            Registry.register(BuiltInRegistries.ITEM, key, item);
+        }
+
+        return Registry.register(BuiltInRegistries.BLOCK, key, block);
+    }
+}
