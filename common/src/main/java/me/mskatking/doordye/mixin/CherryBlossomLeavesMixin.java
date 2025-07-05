2b5204e94391c9de3fd2d1160ec64472e9b4fcc2
MSKatKing <mark@dinoslice.com>
2025-07-05 12:31:44 -0500

    Implement growth speed for cherry leaves

--- common/src/main/java/me/mskatking/doordye/mixin/CherryBlossomLeavesMixin.java
+++ common/src/main/java/me/mskatking/doordye/mixin/CherryBlossomLeavesMixin.java
@@ -1,20 +1,28 @@
 package me.mskatking.doordye.mixin;
 
+import me.mskatking.doordye.item.DoOrDyeItems;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.ItemInteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelReader;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.BonemealableBlock;
-import net.minecraft.world.level.block.CherryLeavesBlock;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.phys.BlockHitResult;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
@@ -57,12 +65,12 @@ public final class CherryBlossomLeavesMixin extends LeavesBlockMixin implements 
 
     @Override
     public boolean isValidBonemealTarget(@NotNull LevelReader levelReader, @NotNull BlockPos blockPos, @NotNull BlockState blockState) {
-        return blockState.getValue(GROWTH_FACTOR) != 5;
+        return blockState.getValue(GROWTH_FACTOR) != 5 || !blockState.getValue(CAN_GROW);
     }
 
     @Override
     public boolean isBonemealSuccess(@NotNull Level level, @NotNull RandomSource randomSource, @NotNull BlockPos blockPos, @NotNull BlockState blockState) {
-        return blockState.getValue(GROWTH_FACTOR) != 5;
+        return blockState.getValue(GROWTH_FACTOR) != 5 || !blockState.getValue(CAN_GROW);
     }
 
     @Override
@@ -70,7 +78,7 @@ public final class CherryBlossomLeavesMixin extends LeavesBlockMixin implements 
         if (!blockState.getValue(CAN_GROW)) {
             serverLevel.setBlockAndUpdate(blockPos, blockState.setValue(CAN_GROW, true));
         } else {
-            serverLevel.setBlockAndUpdate(blockPos, blockState.setValue(GROWTH_FACTOR, Math.min(blockState.getValue(GROWTH_FACTOR) + randomSource.nextInt(2), 5)));
+            serverLevel.setBlockAndUpdate(blockPos, blockState.setValue(GROWTH_FACTOR, Math.min(blockState.getValue(GROWTH_FACTOR) + 1, 5)));
         }
     }
 
@@ -81,9 +89,18 @@ public final class CherryBlossomLeavesMixin extends LeavesBlockMixin implements 
     protected void randomTick(@NotNull BlockState pState, @NotNull ServerLevel pLevel, @NotNull BlockPos pPos, @NotNull RandomSource pRandom) {
         if (pState.getValue(CAN_GROW) && pLevel.getRawBrightness(pPos, 0) >= 9) {
             int age = pState.getValue(GROWTH_FACTOR);
-            if (age < 5 && pRandom.nextInt(10) == 0) {
+            if (age < 5 && pRandom.nextInt(25 - doOrDye$growthSpeed(pLevel, pPos, pState)) == 0) {
                 pLevel.setBlockAndUpdate(pPos, pState.setValue(GROWTH_FACTOR, age + 1));
             }
         }
+    }
+
+    @Unique
+    private int doOrDye$growthSpeed(@NotNull ServerLevel pLevel, @NotNull BlockPos pPos, @NotNull BlockState pState) {
+        int brightness = pLevel.getRawBrightness(pPos, 0);
+        int distance = 7 - pState.getValue(LeavesBlock.DISTANCE);
+        boolean waterlogged = pState.getValue(BlockStateProperties.WATERLOGGED);
+
+        return Math.max(0, brightness + distance - (waterlogged ? 10 : 0));
     }
 }
