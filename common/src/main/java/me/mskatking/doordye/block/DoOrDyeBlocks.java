76ff5630cb81ba957bb54a944dadc945070e1b5e
MSKatKing <mark@dinoslice.com>
2025-07-06 22:03:24 -0500

    Give models to more items and block

--- common/src/main/java/me/mskatking/doordye/block/DoOrDyeBlocks.java
+++ common/src/main/java/me/mskatking/doordye/block/DoOrDyeBlocks.java
@@ -11,7 +11,7 @@ public class DoOrDyeBlocks {
     public static final Block BLUEBERRY_BUSH = CommonBlocks.register("blueberry_bush", BlueberryBushBlock::new, BlockBehaviour.Properties.of(), false);
     public static final Block VANILLA_VINES = CommonBlocks.register("vanilla_vines", VanillaVineBlock::new, BlockBehaviour.Properties.of(), true);
     public static final Block LAVENDER_FLOWER = CommonBlocks.register("lavender_flower", TallFlowerBlock::new, BlockBehaviour.Properties.of().mapColor(MapColor.PLANT).noCollission().instabreak().sound(SoundType.GRASS).offsetType(BlockBehaviour.OffsetType.XZ).ignitedByLava().pushReaction(PushReaction.DESTROY), true);
-    public static final Block CALLA_LILLY = CommonBlocks.register("calla_lilly", prop -> new FlowerBlock(SuspiciousStewEffects.EMPTY, prop), BlockBehaviour.Properties.of(), true);
+    public static final Block CALLA_LILY = CommonBlocks.register("calla_lily", prop -> new FlowerBlock(SuspiciousStewEffects.EMPTY, prop), BlockBehaviour.Properties.of().noCollission().instabreak().sound(SoundType.GRASS).offsetType(BlockBehaviour.OffsetType.XZ).ignitedByLava().pushReaction(PushReaction.DESTROY).dynamicShape(), true);
 
     public static void registerBlocks() { }
 }
