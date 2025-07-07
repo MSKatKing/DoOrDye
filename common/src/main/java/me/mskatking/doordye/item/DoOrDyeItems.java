a4d04cae4ce878ac9f68e85267288c4f7fb81efb
MSKatKing <mark@dinoslice.com>
2025-07-06 20:23:21 -0500

    Remove unnecessary FruitItem class

--- common/src/main/java/me/mskatking/doordye/item/DoOrDyeItems.java
+++ common/src/main/java/me/mskatking/doordye/item/DoOrDyeItems.java
@@ -10,6 +10,7 @@ import net.minecraft.world.item.ItemNameBlockItem;
 
 public class DoOrDyeItems {
     public static final Item CHERRIES = CommonItems.register("cherries", prop -> new FruitItem(prop, 2, 1), new Item.Properties());
+    public static final Item CHERRIES = CommonItems.register("cherries", Item::new, new Item.Properties().food(new FoodProperties.Builder().nutrition(2).saturationModifier(1).build()));
     public static final Item BLUEBERRIES = CommonItems.register("blueberries", prop -> new ItemNameBlockItem(DoOrDyeBlocks.BLUEBERRY_BUSH, prop), new Item.Properties().food(new FoodProperties.Builder().nutrition(2).saturationModifier(1).build()));
     public static final Item VANILLA_BEANS = CommonItems.register("vanilla_beans", Item::new, new Item.Properties());
 
